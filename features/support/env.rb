c0aa230bb3a4adcfa8de153850132d55514f7a9d
Rodrigo Soares <rodrigo.soares@mpgo.mp.br>
2025-01-20 12:12:50 -0300

    configurações de ambiente e relatorios

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,15 @@
 require 'capibara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
+require_relative 'helper.rb'
+
+AMBIENTE = ENV['AMBIENTE']
+CONFIG = YAML.load_file(file.dirname(__FILE__) + "ambientes/#{AMBIENTE}.yml")
+
+World(Helper)
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = 'http://localhost:3000'
+  config.app_host = CONFIG['url_padrao']
   config.default_max_wait_time = 10
 end
